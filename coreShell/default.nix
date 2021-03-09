23b68b98d15cebe9635c3b68b7e2474d5c4f222c
Cyril Cohen <CohenCyril@users.noreply.github.com>
2021-03-09 17:01:34 +0100

    Create default.nix

---
+++ coreShell/default.nix
@@ -0,0 +1,6 @@
+{ config ? {}, withEmacs ? false, print-env ? false, do-nothing ? false,
+  update-nixpkgs ? false, ci ? false, ci-step ? null, inNixShell ? null
+}@args:
+let src = fetchGit "https://github.com/coq-community/nix-toolbox.git"; in
+(import "${src}/auto-config.nix" ./. ({config.coq-attribute = "mathcomp-core-shell"} args)).nix-auto
+
